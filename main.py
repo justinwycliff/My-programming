192662a15b991b58a34df800184ea9c6a464eaa9
justin <justinwycliff@gmail.com>
2024-02-08 16:24:34 +0300

    task completed

---
+++ main.py
@@ -0,0 +1,37 @@
+# my first program 
+print("hello world") 
+
+#declaring an integer
+x = 2
+# inspecting the value of x
+print(x)
+
+#knowing the datatype x
+print(type(x))
+
+#declaring a float
+
+y = 2.5
+# inspecting the value y
+print(y)
+
+#knowing datatype y
+print(type(y))
+
+#declaring a string
+
+name = "justin" 
+z = "2.5"
+#knowing datatype z
+print("justin")
+
+print(name)
+print(type(name))
+
+
+#addition ofnumbers
+r = x + 2
+
+print(r)
+
+
