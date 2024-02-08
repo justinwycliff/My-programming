ddc9345669bc3f55b79b8b126594cfe59d952d88
justin <justinwycliff@gmail.com>
2024-02-08 17:31:10 +0300

    task done

--- main.py
+++ main.py
@@ -34,4 +34,48 @@ r = x + 2
 
 print(r)
 
+#delaring an integer
+m = 5
 
+#inspecting the value of m
+print(m)
+
+#knowing the datatype m
+print(type(m))
+
+#declaring a string
+name = "habiba"
+n = "5.6"
+
+#knowing the datatype n
+print(type(name))
+
+#declaring a float
+z = 2.3
+
+#inspecting value of z
+print(z)
+
+#knowing the datatype z
+print(type(z))
+
+#addition of two variables
+q = m + z
+print(q)
+print(m + z)
+
+#subtraction of two variables
+w = m -z
+print(w)
+
+#division of two variables
+r = m / z
+print(r)
+
+#multiplication of two variables
+y = m * z
+print(y)
+
+#getting the remainder
+5 / 2
+print(5/2)
